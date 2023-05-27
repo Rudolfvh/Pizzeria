520df526683427cf6777e3b3a3708651a5de28c6
Rudolf_vh <89134031+Rudolfvh@users.noreply.github.com>
2023-05-27 10:13:58 +0300

    Commit

--- src/main/java/dto/CreateCustomerDto.java
+++ src/main/java/dto/CreateCustomerDto.java
@@ -3,15 +3,17 @@ package dto;
 import lombok.Builder;
 import lombok.Value;
 
-import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Value
 @Builder
 public class CreateCustomerDto {
-    @Valid
-    String name;
     @NotNull
+    @NotBlank
+    String name;
+    @Pattern(regexp = "^\\+\\d{9,14}")
     String phone;
     @NotNull
     String password;
