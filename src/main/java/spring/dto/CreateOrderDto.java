9fc3f77da78832fa6457fcf2f6f897abe8633b8a
Rudolf_vh <matyane97@gmail.com>
2023-06-07 17:03:53 +0300

    commit

--- src/main/java/spring/dto/CreateOrderDto.java
+++ src/main/java/spring/dto/CreateOrderDto.java
@@ -5,13 +5,12 @@ import spring.entity.Pizza;
 import lombok.Builder;
 import lombok.Value;
 
-import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @Value
 @Builder
 public class CreateOrderDto {
-    @Valid
+
     Customer customer;
     Pizza pizza;
     LocalDateTime dateGet;
